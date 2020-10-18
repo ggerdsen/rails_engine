27f208724903dee586e752c6b944faa3f0d3311f
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-10-17 19:55:04 -0600

    Latest schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,63 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_10_18_003024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customers", force: :cascade do |t|
+    t.integer "csv_id"
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "invoice_items", force: :cascade do |t|
+    t.integer "csv_id"
+    t.integer "item_id"
+    t.integer "invoice_id"
+    t.integer "quantity"
+    t.integer "unit_price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "invoices", force: :cascade do |t|
+    t.integer "csv_id"
+    t.integer "customer_id"
+    t.integer "merchant_id"
+    t.string "status"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer "csv_id"
+    t.string "name"
+    t.string "description"
+    t.integer "unit_price"
+    t.integer "merchant_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "merchants", force: :cascade do |t|
+    t.integer "csv_id"
+    t.string "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.integer "csv_id"
+    t.integer "invoice_id"
+    t.bigint "credit_card_number"
+    t.date "credit_card_expiration_date"
+    t.string "result"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
