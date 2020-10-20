e662a3395a0225aa39f9622da4302a3125cd31e3
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-10-20 16:52:04 -0600

    Create needed routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       # get "/items", to: "items#index"
-      resources :merchants, only: [:index, :show, :create]
+      resources :merchants
+      # , only: [:index, :show, :create, :destroy, :put]
     end
   end
 end
