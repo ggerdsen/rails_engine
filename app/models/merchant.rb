42be6afa0040c144d4f85e9cd84ca47a5bcb6354
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-10-20 16:10:34 -0600

    Adjust model and validations

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,7 +1,5 @@
 class Merchant < ApplicationRecord
-  validates_presence_of :name,
-                        :created_at,
-                        :updated_at
+  validates_presence_of :name
   
   has_many :items
   has_many :invoices
