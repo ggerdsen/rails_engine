feeff70d2723933e699077cfdd5af644f6162d85
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-10-20 17:22:26 -0600

    Add routes for Items

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       # get "/items", to: "items#index"
       resources :merchants
+      resources :items
       # , only: [:index, :show, :create, :destroy, :put]
     end
   end
