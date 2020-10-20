166da1fbaa9d6fd52de52728ab0498605d4f2501
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-10-20 17:47:35 -0600

    Create items and methods for item CRUD functionality.  All tests pass.

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,28 +1,28 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    render json: MerchantSerializer.new(Item.all)
+    render json: ItemSerializer.new(Item.all)
   end
-  #
-  # def show
-  #   render json: MerchantSerializer.new(Merchant.find(params[:id]))
-  # end
-  #
-  # def create
-  #   render json: MerchantSerializer.new(Merchant.create(merchant_params))
-  # end
-  #
-  # def destroy
-  #   render json: MerchantSerializer.new(Merchant.destroy(params[:id]))
-  # end
-  #
-  # def update
-  #   render json: MerchantSerializer.new(Merchant.update(params[:id], merchant_params))
-  # end
-  #
-  # private
-  #
-  # def merchant_params
-  #   params.permit(:name)
-  # end
+  
+  def show
+    render json: ItemSerializer.new(Item.find(params[:id]))
+  end
+  
+  def create
+    render json: ItemSerializer.new(Item.create(item_params))
+  end
+  
+  def destroy
+    render json: ItemSerializer.new(Item.destroy(params[:id]))
+  end
+  
+  def update
+    render json: ItemSerializer.new(Item.update(params[:id], item_params))
+  end
+  
+  private
+  
+  def item_params
+    params.permit(:name, :description, :unit_price, :merchant_id)
+  end
 
 end
