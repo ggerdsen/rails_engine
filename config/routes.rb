0060c806e88a8a6d41b18c0b44a0a9f261e04d57
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-10-22 10:56:07 -0600

    Create namespaced routes for searches

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,15 @@ Rails.application.routes.draw do
     namespace :v1 do
       namespace :merchants do
         get "/:id/items", to: "items#index"
+        get "/find_all", to: "search#index"
+        get "/find", to: "search#show"
       end
       resources :merchants
       
       namespace :items do
         get "/:id/merchant", to: "merchants#index"
+        get "/find_all", to: "search#index"
+        get "/find", to: "search#show"
       end
       resources :items
     end
